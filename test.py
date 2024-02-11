51b01f0d28f27850cf6cff2bdb6048cbb2adea10
coderjhaw <joshuaehd.silvio.37@gmail.com>
2024-02-11 11:54:29 +0800

    modify test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("Hello! Welcome to my Repo")
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql://root:root@localhost:5432/northwind')
+
+pd.read_sql('SELECT * FROM customers')
+
