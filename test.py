719e9a51d81af073049a873f65d5cf6b8ba92e5d
coderjhaw <joshuaehd.silvio.37@gmail.com>
2024-02-12 09:15:48 +0800

    modify script and setup config

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
+import json
 
-engine = create_engine('postgresql://root:root@localhost:5432/northwind')
+# read the json file to python dict
+def get_postgres_credentials(path='config.json'):
+    with open(path, 'r') as file:
+        config = json.load(file)
+    return config['postgres']
+
+# connect to postgres using create_engine
+def connect_to_postgres():
+    credentials = get_postgres_credentials()
+    connection_string = (
+        f"postgresql://{credentials['user']}:{credentials['password']}@{credentials['host']}:{credentials['port']}/{credentials['database']}"
+    )
+    engine = create_engine(connection_string)
+    connection = engine.connect()
+    return connection
+
+engine = connect_to_postgres()
 
 df = pd.read_sql('SELECT * FROM customers', engine)
 
